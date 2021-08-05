1053904b80feb39f0e0953a54639993a84674cbb
Len Hulley <lenhulley@gmail.com>
2021-08-05 09:34:29 +0100

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,46 @@
+using Imageflow.Server;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageFlowBasic
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    context.Response.ContentType = "text/html";
+                    await context.Response.WriteAsync("<img src=\"image.jpg?width=450\" />");
+                });
+            });
+
+            app.UseImageflow(new ImageflowMiddlewareOptions()
+                .SetMapWebRoot(true)
+                 .SetMyOpenSourceProjectUrl("https://github.com/lenhulley/ImageFlowBasic"));
+        }
+    }
+}
